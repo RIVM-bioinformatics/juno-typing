7feac0c655a06eb954f327e3d56905e121b86151
verhager <robert.verhagen@rivm.nl>
2024-12-03 20:14:26 +0100

    add alle values from SeqSero_result.tsv

--- cr_meta.py
+++ cr_meta.py
@@ -5,30 +5,31 @@ import json
 import sys
 import re
 import csv
-
+import pandas as pd
 
 def changetoavus(metas):
     avus=[]
     for meta in metas:
-        #print(meta, metas[meta])
         avus.append({ "units": "", "attr": meta, "value": metas[meta] })
-    #print(avus)
     avus.append({ "units": "", "attr": "pipeline", "value": "juno-typing" })
     avus.append({ "units": "", "attr": "project", "value": "salm" })
     return avus
 
 def main(args):
-    metadata=[]
+    metadata=[]; values={}
+    # walk through all files in output dir
     for filename in pathlib.Path(args.output_dir).glob("*/*/*"):
         f=str(filename).split("/")
         if f[len(f)-3] != "log" and f[len(f)-3] != "audit_trail":
-           #print(f[len(f)-3], f)
+
+           # for now we only analize SeqSero_result.tsv, add all values to the standard dict (analysis, samplename, filename)
            if f[len(f)-1] == "SeqSero_result.tsv":
-              with open(filename) as file:
-                  tsv_file = csv.reader(file, delimiter="\t")
-                  for a in tsv_file:
-                      if a[8] != "Predicted serotype":
-                          avus = changetoavus({ 'analysis': f[len(f)-3], 'sample_name': f[len(f)-2], 'filename': f[len(f)-1], 'Predicted serotype': a[8] })
+              df = pd.read_csv(filename, sep='\t')
+              values = {'analysis': f[len(f)-3], 'sample_name': f[len(f)-2], 'filename': f[len(f)-1]}
+              for n in df.columns:
+                  values.update({n: str(df[n][0])})
+              # make irods avus from dict (only use attribute and value)
+              avus = changetoavus(values)
            else:
               avus = changetoavus({ 'analysis': f[len(f)-3], 'sample_name': f[len(f)-2], 'filename': f[len(f)-1] })
            metadata.append( { 'path': str(os.path.relpath(filename, args.output_dir)), "type": "dataobject", 'metadata': avus })
